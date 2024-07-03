d7d757af4a6de244a076447381fd8284b18f4adf
joaocastro <joaocastro0429@gmail.com>
2024-07-02 22:58:15 -0300

    add models

--- fundamentos/app/models.py
+++ fundamentos/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome=models.CharField(max_length=100, null=False, blank=False)
+    idade=models.IntegerField(null=False,blank=False)
+    data_nascimento=models.DateTimeField(null=False,blank=False)
+    is_ativo=models.BooleanField(null=False,default=True)
