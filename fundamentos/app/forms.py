680634b6c51ed5dd2405214bf9ec12f7f3e4da75
joaocastro <joaocastro0429@gmail.com>
2025-02-03 20:43:51 -0300

    update do projeto

--- fundamentos/app/forms.py
+++ fundamentos/app/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from .models import Cliente
 
 class ClientForm(forms.ModelForm):
-    # nome = forms.CharField(label="Nome do cliente", max_length=100)
-    # idade = forms.IntegerField(label="Informe a sua idade")
-    # data_nascimento = forms.DateField(label="Data de nascimento")
-    # is_ativo = forms.BooleanField(label="O cliente está ativo?")
+     nome = forms.CharField(label="Nome do cliente", max_length=100)
+     idade = forms.IntegerField(label="Informe a sua idade")
+     data_nascimento = forms.DateField(label="Data de nascimento")
+     is_ativo = forms.BooleanField(label="O cliente está ativo?")
 
-    class Meta:
+     class Meta:
         model=Cliente
         fields=('nome','idade','data_nascimento','is_ativo')
